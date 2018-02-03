8597f46289d24623ffcff3a8b9f318a2f974a1e2
zey <benblaise930@gmail.com>
2018-02-03 16:21:32 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1517671176,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/mcdealern-sheet0.png",
+		"images/mrlosanger-sheet0.png",
+		"images/mrtriangleur-sheet0.png",
+		"images/118587widescreenaesthetictumblrbackgroundsblack1080x1920forsamsunggalaxy-sheet0.png",
+		"images/0h7earj-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
